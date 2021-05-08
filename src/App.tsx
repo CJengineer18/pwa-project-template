4396a474824b6a136617ccb5726a8c2535257d2b
CJengineer18 <cjjd18.e2@gmail.com>
2021-05-08 15:22:51 -0500

    Update dependencies. Add Readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-// import  { Suspense } from 'react'
+// import { lazy, Suspense } from 'react'
 import { Router } from '@reach/router'
 import Home from './pages/Home'
 import About from './pages/About'
 import Content from './pages/Content'
 
-/*
-const Home = React.lazy(() => import("./pages/Home"));
-const About = React.lazy(() => import("./pages/About"));
-const Content = React.lazy(() => import("./pages/Content"));
-*/
+// const Home = lazy(() => import('./pages/Home'))
+// const About = lazy(() => import('./pages/About'))
+// const Content = lazy(() => import('./pages/Content'))
 
 function App() {
   return (
